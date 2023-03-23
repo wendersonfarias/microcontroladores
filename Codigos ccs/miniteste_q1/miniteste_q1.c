16980cba763a9a5f90382486b417885e6ba652f3
Wendersonfarias <wendersonpfarias@gmail.com>
2023-03-22 23:18:47 -0400

    add novos projetos

---
+++ Codigos ccs/miniteste_q1/miniteste_q1.c
@@ -0,0 +1,74 @@
+//#include "C:\Users\wende\Desktop\MICRO\microcontroladores\Codigos ccs\miniteste_q1\miniteste_q1.h"
+#include <16F877A.h>
+
+
+#device adc=8
+
+#FUSES NOWDT                    //No Watch Dog Timer
+#FUSES HS                       //High speed Osc (> 4mhz for PCM/PCH) (>10mhz for PCD)
+#FUSES NOPUT                    //No Power Up Timer
+#FUSES NOPROTECT                //Code not protected from reading
+#FUSES NODEBUG                  //No Debug mode for ICD
+#FUSES NOBROWNOUT               //No brownout reset
+#FUSES NOLVP                    //No low voltage prgming, B3(PIC16) or B5(PIC18) used for I/O
+#FUSES NOCPD                    //No EE protection
+#FUSES NOWRT                    //Program memory not write protected
+#FUSES RESERVED                 //Used to set the reserved FUSE bits
+
+#use delay(clock=20000000)
+
+#ifndef lcd_enable
+   #define lcd_enable      pin_e1      // pino enable do LCD
+   #define lcd_rs         pin_e2      // pino rs do LCD
+   //#define lcd_rw      pin_e2      // pino rw do LCD
+   #define lcd_d4         pin_d4      // pino de dados d4 do LCD
+   #define lcd_d5         pin_d5      // pino de dados d5 do LCD
+   #define lcd_d6         pin_d6      // pino de dados d6 do LCD
+   #define lcd_d7         pin_d7      // pino de dados d7 do LCD
+#endif
+
+#include <mod_lcd.c>
+
+
+void main()
+{
+   int leitura;
+   float tensao;
+   
+   setup_adc_ports(AN0);
+   setup_adc(ADC_CLOCK_DIV_16);
+   setup_psp(PSP_DISABLED);
+   setup_spi(SPI_SS_DISABLED);
+   setup_timer_0(RTCC_INTERNAL|RTCC_DIV_1);
+   setup_timer_1(T1_DISABLED);
+   setup_timer_2(T2_DISABLED,0,1);
+   setup_comparator(NC_NC_NC_NC);
+   setup_vref(FALSE);
+
+   set_adc_channel(0);
+   delay_ms(1);
+   lcd_ini();
+   delay_ms(10);
+   while(TRUE){
+      leitura = read_adc();
+      tensao  = leitura * 5.0 / 256.0;
+      
+      if(leitura < 102){
+         printf(lcd_escreve, "\f\t IFMT \r\n");
+         printf(lcd_escreve, "\tVALOR BAIXO\r");
+      }
+      if(leitura > 205){
+         printf(lcd_escreve, "\f\tIFMT \r\n");
+         printf(lcd_escreve, "\tVALOR %.2f\r", tensao);
+      }
+      if(leitura > 102 && leitura < 205){
+         printf(lcd_escreve, "\f\tIFMT \r\n");
+         printf(lcd_escreve, "\tVALOR MEDIO\r");
+      }
+      
+      delay_ms(333);
+      
+   
+   }
+
+}
---
+++ Codigos ccs/rx_serial_lm35/rx_serial_lm35.c
@@ -0,0 +1,49 @@
+//#include "C:\Users\wende\Desktop\MICRO\microcontroladores\Codigos ccs\rx_serial_lm35\rx_serial_lm35.h"
+#include <16F877A.h>
+#device adc=8
+
+#FUSES NOWDT                    //No Watch Dog Timer
+#FUSES HS                       //High speed Osc (> 4mhz for PCM/PCH) (>10mhz for PCD)
+#FUSES NOPUT                    //No Power Up Timer
+#FUSES NOPROTECT                //Code not protected from reading
+#FUSES NODEBUG                  //No Debug mode for ICD
+#FUSES NOBROWNOUT               //No brownout reset
+#FUSES NOLVP                    //No low voltage prgming, B3(PIC16) or B5(PIC18) used for I/O
+#FUSES NOCPD                    //No EE protection
+#FUSES NOWRT                    //Program memory not write protected
+#FUSES RESERVED                 //Used to set the reserved FUSE bits
+
+#use delay(clock=20000000)
+#use rs232(baud=1200,parity=N,xmit=PIN_C6,rcv=PIN_C7,bits=8,stream=lm35)
+
+
+
+
+#int_RDA
+void  RDA_isr(void) 
+{
+   putc(getc());
+   
+
+}
+
+
+
+void main()
+{
+
+   setup_adc_ports(NO_ANALOGS);
+   setup_adc(ADC_CLOCK_DIV_2);
+   setup_psp(PSP_DISABLED);
+   setup_spi(SPI_SS_DISABLED);
+   setup_timer_0(RTCC_INTERNAL|RTCC_DIV_1);
+   setup_timer_1(T1_DISABLED);
+   setup_timer_2(T2_DISABLED,0,1);
+   setup_comparator(NC_NC_NC_NC);
+   setup_vref(FALSE);
+   enable_interrupts(INT_RDA);
+   enable_interrupts(GLOBAL);
+
+   while(TRUE);
+
+}
